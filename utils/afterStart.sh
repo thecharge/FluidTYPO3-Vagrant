4c023c93b1d3435e2f5b54be128444f1f51ae685
Xaver Maierhofer <xaver.maierhofer@xwissen.info>
2014-11-26 20:59:15 +0100

    [TASK] Return IP after start

--- utils/afterStart.sh
+++ utils/afterStart.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+# Load tmpfs mount in NFS foilders
 sudo mount -a
+
+# Return IP(s) from the VM
+echo '================================='
+ip addr | grep 'state UP' -A2 | grep 'inet ' | tail -n +2 | awk '{print "IP:\t"$2}'
+echo '================================='

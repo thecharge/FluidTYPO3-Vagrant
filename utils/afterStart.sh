c7169bb8e45f2f9b63ac2dce5e90fe241a2ad068
Xaver Maierhofer <xaver.maierhofer@xwissen.info>
2015-08-23 18:14:16 +0200

    [TASK] Add backend URL TYPO3 to start output

--- utils/afterStart.sh
+++ utils/afterStart.sh
@@ -5,5 +5,6 @@ sudo mount -a
 # Return IP(s) from the VM
 echo '================================='
 ip addr | grep 'state UP' -A2 | grep 'inet ' | tail -n +2 | awk '{print "IP:\t"$2}'
-echo "FQDN/URL:\thttp://`hostname -f`"
+echo "Frontend:\thttp://`hostname -f`"
+echo "Backend:\thttp://`hostname -f`/typo3"
 echo '================================='
